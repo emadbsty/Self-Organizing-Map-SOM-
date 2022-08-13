80ff501ed0a4785e9c99b1c9e826328deb539a2f
Emad Bsty <60713473+emadbsty@users.noreply.github.com>
2022-08-13 18:10:28 +0300

    Add files via upload

---
+++ SOM01/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ANN_Lib;
+namespace SOM01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SOM_Network som_network;
+        SOM_Network.SOM_Nodes nodes;
+        float[] inputs;
+        float lr = 0.5f;
+        Point dim; int radius = 7;
+        static Random rand1 = new Random();
+        static Random rand2 = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int out_num = 20;
+            dim = new Point(out_num, out_num);
+            inputs = new float[2];
+            int input_num = 500;
+            float[,] ii = new float[input_num, 2];
+            for (int i = 0; i < input_num; i += 3)
+            {
+                ii[i, 0] = (float)rand1.Next(100, 125) / 1000.0f;
+                ii[i, 1] = (float)rand1.Next(15, 35) / 100.0f;
+            }
+            for (int i = 1; i < input_num; i += 3)
+            {
+                ii[i, 0] = (float)rand1.Next(135, 165) / 1000.0f;
+                ii[i, 1] = (float)rand1.Next(45, 60) / 100.0f;
+            }
+            for (int i = 2; i < input_num; i += 3)
+            {
+                ii[i, 0] = (float)rand1.Next(175, 215) / 1000.0f;
+                ii[i, 1] = (float)rand1.Next(75, 115) / 100.0f;
+            }
+            som_network = new SOM_Network(dim, inputs.Length);
+            for (int k = 0; k < 5; k++)
+            {
+                for (int j = 0; j < input_num; j++)
+                {
+                    inputs[0] = ii[j, 0]; inputs[1] = ii[j, 1];
+                    for (int i = 0; i <= radius; i++)
+                    {
+                        lr = 0.7f;
+                        som_network.CalcDistance(inputs);
+                        nodes = som_network.Get_BMU();
+                        List<Point> pp = som_network.Get_neighboring(nodes, i);
+                        lr = lr * (float)Math.Exp((float)-i / (float)radius);
+                        som_network.Update(pp, inputs, lr);
+                    }
+                }
+            }
+            int[,] auxx = new int[out_num, out_num];
+            for (int i = 0; i < out_num; i++)
+            {
+                for (int j = 0; j < out_num; j++)
+                {
+                    auxx[j, i] = som_network.Nodes[j, i].color.R;
+                }
+            }
+            imageViewer1.inti(auxx, out_num, out_num, 18, 18);
+        }
+    }
+}
---
+++ SOM01/imageViewer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOM01
+{
+    public partial class imageViewer : UserControl
+    {
+        public imageViewer()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        System.Drawing.SolidBrush b1 = new SolidBrush(Color.White);
+        System.Drawing.SolidBrush b2 = new SolidBrush(Color.Black);
+
+        struct Matrix
+        {
+            public int w, h;
+            public Rectangle[] recf;
+            public void generate(int H,int W,int bw,int bh)
+            {
+                this.w = W;
+                this.h = H;
+                this.recf = new Rectangle[this.w * this.h];
+                int k = 0;
+                for (int i = 0; i < this.w; i++)
+                {
+                    for (int j = 0; j < this.h; j++)
+                    {
+                        this.recf[k] = new Rectangle();
+                        this.recf[k].Location = new System.Drawing.Point
+                            (i * bw + 3, j * bh + 3);
+                        this.recf[k].Size = new System.Drawing.Size(bw, bh);
+                        k++;
+                    }
+                }
+            }
+        }
+        Matrix m;
+        private Panel panel1;
+        int[,] image;
+        public void inti(int[,] Image,int H, int W,int bw,int bh)
+        {
+            m.generate(H, W, bw, bh);
+            image = Image;
+            g = panel1.CreateGraphics(); this.DoubleBuffered = true;
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    b1 = new SolidBrush(Color.FromArgb(image[i, j], image[i, j], image[i, j]));
+                    g.FillRectangle(b1, m.recf[i + m.h * j]);
+                    g.DrawRectangle(new Pen(b2.Color, 0.1f), m.recf[i + m.h * j]);
+                }
+            }
+            this.Paint += new PaintEventHandler(UserControl1_Paint);
+        }
+
+        private void imageViewer_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void UserControl1_Paint(object o, PaintEventArgs e)
+        {
+            //panel1.Width = m.w * 10 + 10;
+            //panel1.Height = m.h * 10 + 10;
+            //this.Width = m.w * 10 + 10;
+            //this.Height = m.w * 10 + 10;
+            for (int i = 0; i < m.h; i++)
+            {
+                for (int j = 0; j < m.w; j++)
+                {
+                        b1 = new SolidBrush(Color.FromArgb(image[i,j],image[i,j],image[i,j]));
+                        g.FillRectangle(b1, m.recf[i + m.h * j]);
+                        g.DrawRectangle(new Pen(b2.Color, 0.1f), m.recf[i + m.h * j]);                   
+                }
+            }         
+        }
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(375, 375);
+            this.panel1.TabIndex = 0;
+            // 
+            // imageViewer
+            // 
+            this.Controls.Add(this.panel1);
+            this.Name = "imageViewer";
+            this.Size = new System.Drawing.Size(380, 380);
+            this.Load += new System.EventHandler(this.imageViewer_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void imageViewer_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
